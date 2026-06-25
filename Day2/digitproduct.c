ab746770e6b8c90f6ec3295d4b68d8d3e789ddf9
rohitbabinaa212-debug <rohitbabinaa212@gmail.com>
2026-06-26 02:30:22 +0530

    DAY2 C programs

---
+++ Day2/digitproduct.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i, n,product = 1;
+    printf("enter the number: ");
+    scanf("%d",&n);
+
+    while(n!=0){
+        product = n%10*product;
+        n=n/10;
+
+    }
+    printf("product of digits of the number = %d", product);
+    
+    return 0;
+}
