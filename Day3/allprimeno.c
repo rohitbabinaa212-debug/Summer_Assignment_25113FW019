0d08c2ab0faaf4c57f6ffea1543e0ac66f36ff78
rohitbabinaa212-debug <rohitbabinaa212@gmail.com>
2026-06-26 02:16:39 +0530

    DAY2 c programs

---
+++ Day3/allprimeno.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int low,high,i,j,flag;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d",&low,&high);
+    printf("Prime numbers between %d and %d are: ",low,high);
+    for(i=low+1;i<high;i++) {
+        flag=0;
+        for(j=2;j<=i/2;j++) {
+            if(i%j==0) {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+            printf("%d ",i);
+    }
+    return 0;
+}
